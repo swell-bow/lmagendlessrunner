546318f3616e1c0dac5b2980a12d5d372d9f33e2
Kurt Herrmann <kherrmann@newbelgium.com>
2018-09-18 15:17:47 -0600

    runner with platform generator

---
+++ Assets/Script/DestroyerScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyerScript : MonoBehaviour {
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Playa")
+        {
+            UnityEngine.SceneManagement.SceneManager.LoadScene("MainScene");
+            return;
+        }
+
+        if (collision.gameObject.transform.parent)
+        {
+            Destroy(collision.gameObject.transform.parent.gameObject);
+        }
+        else
+        {
+            Destroy(collision.gameObject);
+        }
+    }
+}
---
+++ Assets/Script/SpawnScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnScript : MonoBehaviour
+{
+
+
+    public GameObject[] obj;
+    public float spawnMin = 3f;
+    public float spawnMax = 5f;
+
+    // Use this for initialization
+    void Start()
+    {
+        Spawn();
+    }
+
+    void Spawn()
+    {
+        Instantiate(obj[Random.Range(0, obj.Length)], transform.position, Quaternion.identity);
+        Invoke("Spawn", Random.Range(spawnMin, spawnMax));
+    }
+}
